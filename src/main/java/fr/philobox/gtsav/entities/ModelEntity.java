75d5857469eb6c882baa85881181b84657199f45
PhiloJoff <philojoff@gmail.com>
2022-04-27 00:05:58 +0200

    begin model entity

--- src/main/java/fr/philobox/gtsav/entities/ModelEntity.java
+++ src/main/java/fr/philobox/gtsav/entities/ModelEntity.java
@@ -1,4 +1,29 @@
 package fr.philobox.gtsav.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ModelEntity {
+    @Id
+    private UUID id ;
+
+    @Temporal(TemporalType.DATE)
+    private Date dateCreation;
+
+    @Temporal(TemporalType.DATE)
+    private Date dateModification;
+
+
 }
