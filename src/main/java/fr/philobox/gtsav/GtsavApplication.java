88453e6e09d00b43ce9889f742ab697ee7cac914
PhiloJoff <philojoff@gmail.com>
2022-04-26 20:27:04 +0200

    Initial Commit

---
+++ src/main/java/fr/philobox/gtsav/GtsavApplication.java
@@ -0,0 +1,13 @@
+package fr.philobox.gtsav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtsavApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GtsavApplication.class, args);
+    }
+
+}
