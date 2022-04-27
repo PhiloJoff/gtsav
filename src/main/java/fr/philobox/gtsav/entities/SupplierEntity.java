d12d3eea6de37c4b52349c4edb66b07ca2bee6f9
PhiloJoff <philojoff@gmail.com>
2022-04-27 23:38:37 +0200

    Add supplier entity and add relation with model

--- src/main/java/fr/philobox/gtsav/entities/SupplierEntity.java
+++ src/main/java/fr/philobox/gtsav/entities/SupplierEntity.java
@@ -5,33 +5,33 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name="mod_model")
-public class ModelEntity {
+@Table(name="sup_supplier")
+public class SupplierEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id ;
 
-    @NotNull
-    @NotEmpty
+    @NotNull(message = "Name is mandatory")
+    @NotEmpty(message = "Name can't be empty")
     private String name;
 
-    @ManyToOne(fetch = FetchType.EAGER)
-    private SupplierEntity supplier;
+    @OneToMany(mappedBy = "supplier")
+    private List<ModelEntity> models;
 
     @Temporal(TemporalType.DATE)
     private Date dateCreation;
 
     @Temporal(TemporalType.DATE)
     private Date dateModification;
-
-
 }
